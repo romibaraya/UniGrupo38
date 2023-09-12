8e82de108f1f5365cdac36137c47e0136575ac4f
Franco-Massola <francomassola1@gmail.com>
2023-09-11 22:23:51 -0300

    maria db

--- src/unigrupo38/UniGrupo38.java
+++ src/unigrupo38/UniGrupo38.java
@@ -17,7 +17,7 @@ public class UniGrupo38 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("buenas buenas");
-        
+        System.out.println("probando con maria db");
     }
     
 }
